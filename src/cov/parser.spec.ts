3d97463664e2c7f85f3f27bcb00c94a9ae25223d
Sungwoong Ha <hsw1018@berkeley.edu>
2023-02-01 04:27:33 -0800

    Addd type annotaion, Test parse_data

--- src/cov/parser.spec.ts
+++ src/cov/parser.spec.ts
@@ -1,10 +1,32 @@
-import { describe, it, expect, toBe } from 'vitest'
+import { describe, it, expect, test } from 'vitest'
 
-import parse_line from 'cov/parser'
+import {parse_line, parse_data} from './parser'
 
 describe('parser', () => {
   it('should run', () => {
-      console.log(parse_line("C 'falu.vl12n5pagev_line/aluoblockS12hTOP.alu' 109"))
-     //expect(sum(1, 2)).toBe(3);
+      console.log(parse_line("C 'falu.vl12n5pagev_line/aluoblockS12hTOP.alu' 109"));
+      console.log(parse_data("C 'falu.vl12n5pagev_line/aluoblockS12hTOP.alu' 109\nC '\x01f\x02Test1Module.sv\x01l\x0240\x01n\x020\x01page\x02v_user/Test1Module\x01o\x02cover\x01h\x02TOP.Test1Module' 3\nC '\x01f\x02Test1Module.sv\x01l\x028\x01n\x020\x01page\x02v_user/SubModule1\x01o\x02cover\x01h\x02TOP.Test1Module.c0' 0\nC '\x01f\x02Test1Module.sv\x01l\x028\x01n\x020\x01page\x02v_user/SubModule1\x01o\x02cover\x01h\x02TOP.Test1Module.c1' 0"));
+      expect(1+2).toBe(3);
   })
+
+  it('should parse a line', () => {
+    const line = "C '\u0001f\u0002alu.v\u0001l\u000212\u0001n\u00025\u0001page\u0002v_line/alu\u0001o\u0002block\u0001S\u000212\u0001h\u0002TOP.alu' 109";
+    const result = parse_line(line);
+    expect(result.file).toBe('alu.v');
+    expect(result.line).toBe('12');
+    expect(result.path).toBe('v_line/alu');
+    expect(result.count).toBe(109);
+  });
+
+  it('should parse data', () => {
+    const data = "C '\u0001f\u0002alu.v\u0001l\u000212\u0001n\u00025\u0001page\u0002v_line/alu\u0001o\u0002block\u0001S\u000212\u0001h\u0002TOP.alu' 109";
+    const result = parse_data(data);
+    expect(result.length).toBe(1);
+    expect(result[0].file).toBe('alu.v');
+    expect(result[0].line).toBe('12');
+    expect(result[0].path).toBe('v_line/alu');
+    expect(result[0].count).toBe(109);
+  });
 })
+
+
--- src/cov/parser.ts
+++ src/cov/parser.ts
@@ -1,5 +1,11 @@
+export { parse_line,parse_data };
+
 class CodeLine {
-  constructor(line_number, line){
+  line_number: number;
+  line: string;
+  line_count: number;
+
+  constructor(line_number: number, line: string) {
       this.line_number = line_number;
       this.line = line;
       this.line_count = 0;
@@ -7,51 +13,66 @@ class CodeLine {
 }
 
 class CodeBlock {
-  constructor(file_name, code_lines) {
+  file_name: string;
+  code_lines: CodeLine[];
+
+  constructor(file_name: string, code_lines: CodeLine[]) {
       this.file_name = file_name;
       this.code_lines = code_lines;
   }
 }
+
 class CoverageEntry {
-  constructor(file, line, path, count) {
+  file: string;
+  line: string;
+  path: string;
+  count: number;
+
+  constructor(file: string, line: string, path: string, count: number) {
   this.file = file;
   this.line = line;
   this.path = path;
   this.count = count;
-
   }
 }
-function parse_line(line) {
-  var arr = line.split("\'")
 
-  var file = ""
-  var line = ""
-  var path = ""
-  var count = parseInt(arr[2].trim());
+function parse_line(line: string): CoverageEntry {
+  let arr = line.split("\'");
 
-  var arr2 = arr[1].split('\u0001');
-  for (var i = 0; i < arr2.length; i++) {
-      var key_value_pair = arr2[i].split('\u0002')
-      if (key_value_pair[0] === 'f'){
-          file = key_value_pair[1]
+  let file = "";
+  let line_num = "";
+  let path = "";
+  let count=0;
+  let arr2=[];
+  if (arr.length >= 3) {
+    count = parseInt(arr[2].trim(), 10);
+  }
+  if (arr.length >=2){
+    arr2 = arr[1].split("\u0001");
+  }
+  for (const i in arr2) {
+    const keyValuePair = arr2[i].split("\u0002");
+    if (keyValuePair[0] === "f") {
+      file = keyValuePair[1];
       }
-      if (key_value_pair[0] === 'l'){
-          line = key_value_pair[1]
+    if (keyValuePair[0] === "l") {
+      line_num = keyValuePair[1];
       }
-      if (key_value_pair[0] === 'page'){
-          path = key_value_pair[1]
+    if (keyValuePair[0] === "page") {
+      path = keyValuePair[1];
       }
   }
-  const new_entry = new CoverageEntry(file, line, path, count);
-  return new_entry
+  let newEntry = new CoverageEntry(file, line_num, path, count);
+  return newEntry;
 }
 
-function parse_data(data) {
-  var parsed_lines = [];
-  const lines = data.split("\n");
-  for (var i = 1; i < lines.length-1; ++i) {
-      parsed_lines.push(parse_line(lines[i]));
+function parse_data(data: string): CoverageEntry[] {
+  let parsedLines: CoverageEntry[] = [];
+  let lines = data.split("\n");
+  for (let i = 0; i < lines.length; ++i) {
+    parsedLines.push(parse_line(lines[i]));
   }
-  return parsed_lines;
+  return parsedLines;
 }
-module.exports = parse_line;
+
+export default parse_line;
